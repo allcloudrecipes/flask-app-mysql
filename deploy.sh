82c65cc4e209ea5bccf7eb15b9d429192a5e4ca0
sahbijabnouni <sahbi.jabnouni@tylertech.com>
2019-10-27 18:57:17 -0400

    --save changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 sudo yum update -y
 sudo yum install git -y
-sudo yum install mysql
+sudo yum install mysql -y
 sudo yum -y install python-pip
 sudo pip install flask
 sudo pip install pymysql
 sudo pip install Flask-Table
 sudo pip install flask-mysql
 sudo git clone https://github.com/allcloudrecipes/flask-app-mysql.git
-sudo python . /flask-app-mysql/main.py
+sudo python flask-app-mysql/main.py
