d396e5d1fc82c0e7036ef34983605d37fdd3679e
sahbijabnouni <sahbi.jabnouni@tylertech.com>
2019-11-06 09:32:04 -0500

    --save changes

--- scripts/scripts/install_dependencies.sh
+++ scripts/scripts/install_dependencies.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install python3 -y
-sudo yum install mysql -y
 sudo yum install python-pip3 -y
+sudo pip3 install virtualenv
+virtualenv /home/ec2-user/flask-app-mysql/flaskappenv
+source /home/ec2-user/flask-app-mysql/flaskappenv/bin/activate
 sudo pip3 install flask
 sudo pip3 install pymysql
 sudo pip3 install Flask-Table
