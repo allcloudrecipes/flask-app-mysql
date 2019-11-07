2375b88c05bed77cabcaced9af4bbe20a41c8eea
sahbijabnouni <sahbi.jabnouni@tylertech.com>
2019-11-07 09:29:44 -0500

    --save changes

--- scripts/deploy_application.sh
+++ scripts/deploy_application.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo pip3 install virtualenv
+pip3 install virtualenv
 virtualenv /home/ec2-user/flask-app-mysql/flaskappenv
 source /home/ec2-user/flask-app-mysql/flaskappenv/bin/activate
-sudo pip3 install flask
-sudo pip3 install pymysql
-sudo pip3 install Flask-Table
-sudo pip3 install flask-mysql
-sudo pip3 install gunicorn
-sudo cp /home/ec2-user/flask-app-mysql/flask-app.service /etc/systemd/system/
-sudo systemctl start flask-app
-sudo systemctl enable flask-app
+pip3 install flask
+pip3 install pymysql
+pip3 install Flask-Table
+pip3 install flask-mysql
+pip3 install gunicorn
+cp /home/ec2-user/flask-app-mysql/flask-app.service /etc/systemd/system/
+systemctl start flask-app
+systemctl enable flask-app
